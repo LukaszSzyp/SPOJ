85c23440ad429b573d2670e8e4d02a91697569f3
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2019-08-20 21:29:23 +0200

    add Zadanie Probne:)

---
+++ src/ZadanieProbne.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ZadanieProbne {
+    public static void zadanieProbne() {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.print(a+b);
+    }
+}

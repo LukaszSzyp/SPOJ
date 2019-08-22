757fc3ad4384dbc6fa177dc1fdfb1aa131496a8f
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2019-08-22 20:35:15 +0200

    add czy umiesz potegowac

--- src/CzyUmieszPotegowac.java
+++ src/CzyUmieszPotegowac.java
@@ -1,15 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.*;
+
 
 public class CzyUmieszPotegowac {
 
-    public static void czyUmieszPotegowac() {
+    public static void main(String[] arg) throws java.lang.Exception {
         int[][] cyfry = {{0, 0, 0, 0}, {1, 1, 1, 1}, {2, 4, 8, 6}, {3, 9, 7, 1}, {4, 6, 4, 6}, {5, 5, 5, 5}, {6, 6, 6, 6}, {7, 9, 3, 1}, {8, 4, 2, 6}, {9, 1, 9, 1}};
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.valueOf(scan.readLine());
         int[] wynik = new int[n];
         for (int i = 0; i < n; i++) {
-            int a = scan.nextInt();
-            int b = scan.nextInt();
+            int a = 0;
+            int b = 0;
+            String[] dane = scan.readLine().split(" ");
+            a = Integer.valueOf(dane[0]);
+            b = Integer.valueOf(dane[1]);
             a = a % 10;
             b = b % 4 - 1;
             wynik[i] = cyfry[a][b];
@@ -19,3 +25,5 @@ public class CzyUmieszPotegowac {
         }
     }
 }
+
+

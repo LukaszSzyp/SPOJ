21ceae58a9b32dd97c53ab9bbed7c1cd2b491850
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2019-08-22 21:11:06 +0200

    Działa predkosc srednia

--- src/PredkoscSrednia.java
+++ src/PredkoscSrednia.java
@@ -1,12 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.*;
 
 public class PredkoscSrednia {
     public static void main(String[] arg) throws java.lang.Exception {
         BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.valueOf(bReader.readLine());
         int[] liczby = new int[n];
-        for (int i = 0; i < 2 ; i++) {
+        for (int i = 0; i < n ; i++) {
             int a =0;
             int b =0;
             String[] dane = bReader.readLine().split(" ");
